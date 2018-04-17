4fd72873d5e3ac13cdf7e08b0bfe82bab07a3a6a
Luciano Teixeira <luciano.teixeiras@gmail.com>
2018-04-17 17:31:12 -0300

    Commit Iniciando variaveis

---
+++ AluraCursoJavaParte-I_Eclipse/src/TesteVariaveis.java
@@ -0,0 +1,22 @@
+
+public class TesteVariaveis {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("New Test!!");
+		
+		int idade;
+		idade = 37;
+		
+		System.out.println(idade);
+		
+		idade = idade + 10;
+		
+		System.out.println(idade);
+		
+		idade = (7 * 7) + 2;
+		
+		System.out.println(idade);
+	}
+
+}
