06640a6ed88efefec596830151866c8001f7845e
Luciano Teixeira <luciano.teixeiras@gmail.com>
2018-05-07 17:16:12 -0300

    Commit por hoje

--- AluraCursoJavaParte-I_Eclipse/src/Aula06_Condicionais/Condicionais02.java
+++ AluraCursoJavaParte-I_Eclipse/src/Aula06_Condicionais/Condicionais02.java
@@ -4,8 +4,21 @@ public class Condicionais02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		System.out.println("Testando Condicionais II");
+
+		int idade = 18;
 		
-		
+		int quantidadePessoas = 0;
+
+		if (idade >= 18 || quantidadePessoas >= 3) {
+
+			System.out.println("Seja bem vindo!!!");
+
+		} else {
+
+			System.out.println("Infelizmente você não pode entrar!!!");
+		}
 
 	}
 
