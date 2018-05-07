4b1478615b16db58d6066c1666de41556972cba3
Luciano Teixeira <luciano.teixeiras@gmail.com>
2018-05-07 17:31:48 -0300

    Commit novas condicionais

---
+++ AluraCursoJavaParte-I_Eclipse/src/Aula06_Condicionais/Condicionais04.java
@@ -0,0 +1,25 @@
+package Aula06_Condicionais;
+
+public class Condicionais04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Testando Condicionais III");
+
+		int idade = 16;
+		
+		boolean acompanhado = true;
+
+		if (idade >= 18 && acompanhado) {
+
+			System.out.println("Seja bem vindo!!!");
+
+		} else {
+
+			System.out.println("Infelizmente você não pode entrar!!!");
+		}
+
+	}
+
+}
